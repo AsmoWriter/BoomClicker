f506e6060095644497cf13daf7c0fc40fff369d7
Pavel Korn <lyavolpe@gmail.com>
2020-04-01 18:16:18 +0300

    Add Timer and Health Kit
    Добавлены префабы для аптечек и и таймера. Также новые префабы протеганы и добавлены в список скрипта рейкаста.

--- Assets/Scripts/ReyDestroyer.cs
+++ Assets/Scripts/ReyDestroyer.cs
@@ -6,9 +6,19 @@ public class ReyDestroyer : MonoBehaviour
     private CounterScriptableObject _scoreCounter;
     [SerializeField]
     private GameObject _plusScorePrefab;
+    [SerializeField]
+    private CounterScriptableObject _hpCounter;
+    [SerializeField]
+    private GameObject _plusHpPrefab;
+    [SerializeField]
+    private CounterScriptableObject _timerCounter;
+    [SerializeField]
+    private GameObject _plusTimePrefab;
 
     private bool _keyPressed;
-    private string _destroyTag = "BombTag";
+    private string _bombTag = "BombTag";
+    private string _timerTag = "TimerTag";
+    private string _healthKitTag = "HealthKitTag";
 
     private void Update()
     {
@@ -21,12 +31,24 @@ public class ReyDestroyer : MonoBehaviour
         RaycastHit2D _hit = Physics2D.Raycast(ray, Vector2.zero);
         if (_hit.transform != null)
         {
-           
-            if (_hit.transform.tag == _destroyTag)
+
+            if (_hit.transform.tag == _bombTag)
             {
                 Destroy(_hit.transform.gameObject);
                 Instantiate(_plusScorePrefab, ray, Quaternion.identity);
                 _scoreCounter.ChangeValue(1);
+            }
+            if (_hit.transform.tag == _timerTag)
+            {
+                Destroy(_hit.transform.gameObject);
+                Instantiate(_plusTimePrefab, ray, Quaternion.identity);
+                _timerCounter.ChangeValue(25);
+            }
+            if (_hit.transform.tag == _healthKitTag)
+            {
+                Destroy(_hit.transform.gameObject);
+                Instantiate(_plusHpPrefab, ray, Quaternion.identity);
+                _hpCounter.ChangeValue(10);
             }
         }
     }
