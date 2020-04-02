df770558da442497e06c3000d53da84c5b7b3f29
Pavel Korn <lyavolpe@gmail.com>
2020-04-02 14:16:56 +0300

    Menu control
    Добавлены HUD и назначены действия кнопок из меню игры

--- Assets/Scripts/MenuStartGame.cs
+++ Assets/Scripts/MenuStartGame.cs
@@ -3,8 +3,41 @@ using UnityEngine.SceneManagement;
 
 public class MenuStartGame : MonoBehaviour
 {
-    public void OnStartButtonClick()
+    [SerializeField]
+    private GameObject _bombSpawner;
+    [SerializeField]
+    private GameObject _timerModeSpawner;
+    [SerializeField]
+    private GameObject _healthModeSpawner;
+    [SerializeField]
+    private GameObject _aboutGameWindow;
+
+    [SerializeField]
+    private GameObject _timerModeHUD;
+    [SerializeField]
+    private GameObject _healthModeHUD;
+
+
+    public void OnStarTimerModetButtonClick()
     {
-        SceneManager.LoadScene("MineScene", LoadSceneMode.Single);
+        gameObject.SetActive(false);
+        _timerModeHUD.SetActive(true);
+        _bombSpawner.SetActive(true);
+        _timerModeSpawner.SetActive(true);
+    }
+    public void OnStarHealthModetButtonClick()
+    {
+        gameObject.SetActive(false);
+        _healthModeHUD.SetActive(true);
+        _bombSpawner.SetActive(true);
+        _healthModeSpawner.SetActive(true);
+    }
+    public void OnAboutGameButtonClick()
+    {
+        _aboutGameWindow.SetActive(true);
+    }
+    public void OnCloseAboutGameButtonClick()
+    {
+        _aboutGameWindow.SetActive(false);
     }
 }
